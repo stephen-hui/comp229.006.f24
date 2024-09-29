d23226c7f86715c6739e999a376c4c0d7ec61a57
stephen-hui <huilt1232@gmail.com>
2024-09-28 22:37:34 -0400

    fix navigation bug

--- assignment-1/src/pages/Home.js
+++ assignment-1/src/pages/Home.js
@@ -1,4 +1,5 @@
 // File: Home.js Name: Lit Tung Hui Student ID: 301387861 Date: 2024-09-28
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -11,11 +12,12 @@ export default function Home() {
         </p>
         <p>
           To know more about me, you can visit my{" "}
-          <a href="/about">About page</a>.
+          <NavLink to="/about">About page</NavLink>.
         </p>
         <p>
           You can find me on <a href="https://github.com/stephen-hui">GitHub</a>{" "}
-          and some of my projects on <a href="/projects">Projects page</a>.
+          and some of my projects on{" "}
+          <NavLink to="/projects">Projects page</NavLink>.
         </p>
         <p className="text-green">
           <b> Coding is fun! </b>
